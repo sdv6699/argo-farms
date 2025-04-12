e45b8cfc92e71d97825c4cf32d57e68dc5914f9c
Dheeraj <sahdheeraj518@gmail.com>
2025-04-13 00:55:28 +0530

    fix: initial change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -88,7 +87,6 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
